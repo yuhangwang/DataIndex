29c78f08687c5e4b0834bf215d3feb78d9ca5fd9
Yuhang(Steven) Wang <stevenaura@live.com>
2016-10-09 13:31:37 -0500

    add data/

--- executable/Main.hs
+++ executable/Main.hs
@@ -17,8 +17,20 @@ exe [] = do
 exe [_] = do 
     putStrLn usage
     exitFailure
+exe [_,_] = do 
+    putStrLn usage
+    exitFailure
 
-exe (output:input:_) = do
-    putStrLn output
-    putStrLn input
-    putStrLn hello
+exe (output:input:time_unit:_) = do
+    data <- loadData input
+    let unit = read input :: Float
+    save output (addTime data unit)
+
+loadData :: Num a => FilePath -> IO [[Float]]
+loadData file_path = fmap ((fmap read) . words) (lines (readFile file_path))
+
+save :: String -> [[a]] -> IO ()
+save output_file data = do
+    out = openFile output_file WriteMode
+    hPrint out data
+    hClose out
--- test-suite/Spec_addTime.hs
+++ test-suite/Spec_addTime.hs
@@ -4,5 +4,14 @@ import Test.Tasty.Hspec
 
 test_addTime :: Spec
 test_addTime = parallel $ do
-    it "should return []" $ do
+    it "addTime [] 1. should return []" $ do
         (addTime [] (1.0::Double)) `shouldBe` []
+
+    it "addTime [1,2] 1 should return [[1,1],[2,2]]" $ do
+        (addTime [1,2] (1::Int)) `shouldBe` [[1,1],[2,2]]
+
+    it "addTime [1,2] 2 should return [[2,1],[4,2]]" $ do
+        (addTime [1,2] (2::Int)) `shouldBe` [[2,1],[4,2]]
+
+    it "addTime [1.0,2.0] 1 should return [[1.0,1.0],[2.0,2.0]]" $ do
+        (addTime [1.0,2.0] (1.0::Float)) `shouldBe` [[1.0,1.0],[2.0,2.0]]
