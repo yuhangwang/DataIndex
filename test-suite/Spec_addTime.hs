fd2d8701e554015732a20a9a1c1105384652ad02
Yuhang(Steven) Wang <stevenaura@live.com>
2016-10-09 13:48:39 -0500

    add one more check for 2D input

--- test-suite/Spec_addTime.hs
+++ test-suite/Spec_addTime.hs
@@ -10,6 +10,9 @@ test_addTime = parallel $ do
     it "addTime [[1],[2]] 1 should return [[1,1],[2,2]]" $ do
         (addTime [[1],[2]] (1::Int)) `shouldBe` [[1,1],[2,2]]
 
+    it "addTime [[1,10],[2,20]] 1 should return [[1,1,10],[2,2,20]]" $ do
+        (addTime [[1,10],[2,20]] (1::Int)) `shouldBe` [[1,1,10],[2,2,20]]
+
     it "addTime [[1],[2]] 2 should return [[2,1],[4,2]]" $ do
         (addTime [[1],[2]] (2::Int)) `shouldBe` [[2,1],[4,2]]
 
